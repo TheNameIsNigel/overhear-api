e71c8a6fba8ea568c08e87fe963fc4f7788e01ce
Aidan Follestad <drummer.aidan@gmail.com>
2013-02-22 22:30:46 -0600

    Progress towards finishing playlists.

--- src/com/afollestad/overhearapi/Playlist.java
+++ src/com/afollestad/overhearapi/Playlist.java
@@ -13,11 +13,11 @@ import java.util.Calendar;
 
 public class Playlist {
 
-	private Playlist() {
+    private Playlist() {
     }
 
     private long _id;
-	private String name;
+    private String name;
     private String data;
     private long dateAdded;
     private long dateModified;
@@ -47,65 +47,66 @@ public class Playlist {
     }
 
 
-	public JSONObject getJSON() {
-		JSONObject json = new JSONObject();
-		try {
+    public JSONObject getJSON() {
+        JSONObject json = new JSONObject();
+        try {
             json.put("_id", this._id);
             json.put("_data", this.data);
-			json.put("name", this.name);
-			json.put("date_added", this.dateAdded);
+            json.put("name", this.name);
+            json.put("date_added", this.dateAdded);
             json.put("date_modified", this.dateModified);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return json;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 
     public Uri getSongUri() {
         return MediaStore.Audio.Playlists.Members.getContentUri("external", getId());
     }
 
     public ArrayList<Song> getSongs(Context context) {
-        ArrayList<Song> toreturn = Song.getAllFromUri(context, getSongUri(), null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
-        for(int i = 0; i < toreturn.size(); i++) {
+        ArrayList<Song> toreturn = Song.getAllFromUri(context, getSongUri(), null, null);
+        for (int i = 0; i < toreturn.size(); i++) {
             toreturn.set(i, toreturn.get(i).setFromPlaylist(getId()));
         }
         return toreturn;
     }
 
-	public static Playlist fromJSON(String json) {
-		try {
-			return fromJSON(new JSONObject(json));
-		} catch (JSONException e) {
-			e.printStackTrace();
-			throw new Error(e.getMessage());
-		}
-	}
-	public static Playlist fromJSON(JSONObject json) {
-		Playlist playlist = new Playlist();
-		try {
+    public static Playlist fromJSON(String json) {
+        try {
+            return fromJSON(new JSONObject(json));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public static Playlist fromJSON(JSONObject json) {
+        Playlist playlist = new Playlist();
+        try {
             playlist._id = json.getLong("_id");
             playlist.data = json.getString("_data");
-			playlist.name = json.getString("name");
+            playlist.name = json.getString("name");
             playlist.dateAdded = json.getLong("date_added");
             playlist.dateModified = json.getLong("date_modified");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return playlist;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return playlist;
+    }
 
-	public static Playlist fromCursor(Cursor cursor) {
-		Playlist playlist = new Playlist();
+    public static Playlist fromCursor(Cursor cursor) {
+        Playlist playlist = new Playlist();
 
-		playlist._id = cursor.getLong(cursor.getColumnIndex("_id"));
+        playlist._id = cursor.getLong(cursor.getColumnIndex("_id"));
         playlist.data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.PlaylistsColumns.DATA));
         playlist.name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.PlaylistsColumns.NAME));
         playlist.dateAdded = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.PlaylistsColumns.DATE_ADDED));
         playlist.dateModified = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.PlaylistsColumns.DATE_MODIFIED));
 
-		return playlist;
-	}
+        return playlist;
+    }
 
     public void insertSong(Context context, Song song) {
         Uri uri = MediaStore.Audio.Playlists.Members.getContentUri("external", getId());
@@ -120,7 +121,7 @@ public class Playlist {
     }
 
     public void insertSongs(Context context, ArrayList<Song> songs) {
-        for(Song s : songs) {
+        for (Song s : songs) {
             insertSong(context, s);
         }
     }
@@ -133,17 +134,31 @@ public class Playlist {
         ArrayList<Playlist> toreturn = new ArrayList<Playlist>();
         Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, null,
                 null, null, MediaStore.Audio.Playlists.DEFAULT_SORT_ORDER);
-        while(cursor.moveToNext()) {
+        while (cursor.moveToNext()) {
             toreturn.add(Playlist.fromCursor(cursor));
         }
         cursor.close();
         return toreturn;
     }
 
+    public static Playlist get(Context context, long id) {
+        Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, null,
+                "_id = " + id, null, null);
+        if(!cursor.moveToFirst()) {
+            return null;
+        }
+        Playlist toreturn = Playlist.fromCursor(cursor);
+        cursor.close();
+        return toreturn;
+    }
+
     public static Playlist create(Context context, String name) {
+        if (name == null || name.trim().isEmpty()) {
+            return null;
+        }
         ContentValues values = new ContentValues();
         Calendar now = Calendar.getInstance();
-        values.put(MediaStore.Audio.Playlists.NAME, name);
+        values.put(MediaStore.Audio.Playlists.NAME, name.trim());
         values.put(MediaStore.Audio.Playlists.DATE_ADDED, now.getTimeInMillis());
         values.put(MediaStore.Audio.Playlists.DATE_MODIFIED, now.getTimeInMillis());
         Uri uri = context.getContentResolver().insert(MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, values);
