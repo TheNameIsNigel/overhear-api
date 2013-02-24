870256d7f75716aae8c930bbb22c0677f34cf07b
Aidan Follestad <drummer.aidan@gmail.com>
2013-02-24 17:39:55 -0600

    Updates.

--- src/com/afollestad/overhearapi/Artist.java
+++ src/com/afollestad/overhearapi/Artist.java
@@ -14,28 +14,50 @@ public class Artist {
 		this.key = key;
 	}
 
+	private long _id;
 	private String name;
 	private String key;
+	private int numAlbums;
+	private int numTracks;
 
+	public long getId() {
+		return _id;
+	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public String getKey() {
 		return key;
+	}
+	
+	public int getAlbumCount() {
+		return numAlbums;
+	}
+	
+	public int getTrackCount() {
+		return numTracks;
 	}
 
 	public static Artist fromCursor(Cursor cursor) {
 		Artist album = new Artist();
+		album._id = cursor.getLong(cursor.getColumnIndex("_id"));
 		album.name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.ArtistColumns.ARTIST));
 		album.key = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.ArtistColumns.ARTIST_KEY));
+		album.numAlbums = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.ArtistColumns.NUMBER_OF_ALBUMS));
+		album.numTracks = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.ArtistColumns.NUMBER_OF_TRACKS));
 		return album;
 	}
 
 	public JSONObject getJSON() {
 		JSONObject json = new JSONObject();
 		try {
-			json.put("name", this.name);
-			json.put("key", this.key);
+			json.put("_id", this._id);
+			json.put(MediaStore.Audio.ArtistColumns.ARTIST, this.name);
+			json.put(MediaStore.Audio.ArtistColumns.ARTIST_KEY, this.key);
+			json.put(MediaStore.Audio.ArtistColumns.NUMBER_OF_ALBUMS, this.numAlbums);
+			json.put(MediaStore.Audio.ArtistColumns.NUMBER_OF_TRACKS, this.numTracks);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -45,8 +67,11 @@ public class Artist {
 	public static Artist fromJSON(JSONObject json) {
 		Artist artist = new Artist();
 		try {
-			artist.name = json.getString("name");
-			artist.key = json.getString("key");
+			artist._id = json.getLong("_id");
+			artist.name = json.getString(MediaStore.Audio.ArtistColumns.ARTIST);
+			artist.key = json.getString(MediaStore.Audio.ArtistColumns.ARTIST_KEY);
+			artist.numAlbums = json.getInt(MediaStore.Audio.ArtistColumns.NUMBER_OF_ALBUMS);
+			artist.numTracks = json.getInt(MediaStore.Audio.ArtistColumns.NUMBER_OF_TRACKS);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
